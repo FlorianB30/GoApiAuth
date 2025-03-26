e1aaf9aa0c776b062c0362cc68ee589b530d0cc1
Flo <96230889+FlorianB30@users.noreply.github.com>
2025-03-26 13:00:13 +0100

    Update auth.go

--- controllers/auth.go
+++ controllers/auth.go
@@ -87,13 +87,13 @@ func Login(c *gin.Context) {
 
 	var user models.User
 	if err := config.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
 		return
 	}
 
 	// Check if password matches
 	if !utils.CheckPassword(user.Password, input.Password) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
 		//c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials"})
 		return
 	}

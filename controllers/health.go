dfe61373ee28b3b5ad6a4cc28d5e5186c315f438
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2025-03-26 11:54:51 +0100

    fix json

--- controllers/health.go
+++ controllers/health.go
@@ -1,13 +1,12 @@
 package controllers
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-// Get Health Code 200
-func GetHealth(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK) 
-	fmt.Fprintf(w, "Requête réussie, code 200!")
+func GetHealth(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Healthy",
+	})
 }
-
